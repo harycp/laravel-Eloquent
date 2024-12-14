537e00027410bcdc08e2bab72d539c78ba73bd28
Hary Capri <76942747+harycp@users.noreply.github.com>
2024-12-14 10:38:42 +0700

    feat: initialize Model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    protected $table = 'categories';
+    protected $primaryKey = 'id';
+    protected $keyType = 'string';
+    public $incrementing = false;
+    public $timestamps = false;
 }
