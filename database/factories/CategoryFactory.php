e922b295bd819df8e3a1b5b547c438a304c9dba8
Hary Capri <76942747+harycp@users.noreply.github.com>
2024-12-14 12:01:44 +0700

    feat: add factory faker and seeder for category

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
+ */
+class CategoryFactory extends Factory
+{
+    protected $model = Category::class;
+
+    public function definition()
+    {
+        return [
+            'id' => Str::uuid(),
+            'name' => $this->faker->word(),
+            'description' => $this->faker->sentence(), 
+        ];
+    }
+}
